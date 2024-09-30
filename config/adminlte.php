0c90a5144b9027bced3b08fb25b924bc18b2a647
Tamara Villanueva <villatami@gmail.com>
2024-09-30 03:26:02 -0300

    cambio5

--- config/adminlte.php
+++ config/adminlte.php
@@ -316,6 +316,13 @@ return [
             'can' => 'manage-blog',
         ],
         [
+            'text'        => 'Categorias',
+            'route'       => 'categoria.index',
+            // 'route' => 'proveedor.index',
+            'icon'        => 'fas fa-project-diagram',
+           // 'can'         => 'lista_ventas' //permiso de admin<i class="far fa-project-diagram"></i>
+        ],
+        [
             'text' => 'Mis productos',
             'route' => 'producto.index', // ruta de inicio del crud productos
             'icon' => 'fas fa-store',
