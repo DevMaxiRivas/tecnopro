be6e40403f5b34c1bc404a96fe8292a4684653d6
MR PC <maximilianorivas01@gmail.com>
2024-09-29 23:52:05 -0300

    Restriccion Productos Activo

--- app/Http/Controllers/PaginaDeInicio.php
+++ app/Http/Controllers/PaginaDeInicio.php
@@ -10,11 +10,11 @@ class PaginaDeInicio extends Controller
 {
     public function MandarDatosPaginaInicio()
     {
-        $productos = Producto::latest()->get();
+        $productos = Producto::where('activo', Producto::ACTIVO)->latest()->get();
 
-        $categorias = Categoria::get();
+        $categorias = Categoria::where('activo', Categoria::ACTIVO)->get();
 
-        $carrusel = Producto::inRandomOrder()->take(3)->get();
+        $carrusel = Producto::where('activo', Producto::ACTIVO)->inRandomOrder()->take(3)->get();
 
         return view('frontend.index', compact('productos', 'categorias', 'carrusel'));
     }
@@ -22,20 +22,21 @@ class PaginaDeInicio extends Controller
     public function resultadosBusqueda(Request $request)
     {
 
-        $categorias = Categoria::get();
+        $categorias = Categoria::where('activo', Categoria::ACTIVO)->get();
 
         $terminoBusqueda = $request->input('busqueda');
-        $productosResultados = Producto::where('nombre', 'like', '%' . $terminoBusqueda . '%')->simplePaginate(12)->withQueryString();
+        $productosResultados = Producto::where('activo', Producto::ACTIVO)->where('nombre', 'like', '%' . $terminoBusqueda . '%')->simplePaginate(12)->withQueryString();
 
         return view('frontend.paginas.resultados_busqueda', compact('categorias', 'productosResultados', 'terminoBusqueda'));
     }
 
     public function MandarDatosLista()
     {
-        $categorias = Categoria::get();
+        $categorias = Categoria::where('activo', Categoria::ACTIVO)->get();
 
 
-        $productos = Producto::latest()
+        $productos = Producto::where('activo', Producto::ACTIVO)
+            ->latest()
             ->simplePaginate(24)
             ->withQueryString();
 
@@ -44,14 +45,15 @@ class PaginaDeInicio extends Controller
 
     public function MandarDatosCategoriaEspecifica($variable)
     {
-        $productos = Producto::latest()->take(2)->get();
-        $categorias = Categoria::get();
+        $productos = Producto::where('activo', Producto::ACTIVO)->latest()->take(2)->get();
+        $categorias = Categoria::where('activo', Categoria::ACTIVO)->get();
 
-        $categoria = Categoria::where('id', $variable)->first();
+        $categoria = Categoria::where('activo', Categoria::ACTIVO)->where('id', $variable)->first();
         $categoriaEspecifica = $categoria->nombre;
 
 
-        $productos_especificos = Producto::where('id_categoria', $variable)
+        $productos_especificos = Producto::where('activo', Producto::ACTIVO)
+            ->where('id_categoria', $variable)
             ->latest()
             ->simplePaginate(12)
             ->withQueryString();
@@ -61,22 +63,23 @@ class PaginaDeInicio extends Controller
 
     public function MandarDatosProductoEspecifico($id)
     {
-        $producto = Producto::find($id);
-        $categorias = Categoria::get();
+        $producto = Producto::where('activo', Producto::ACTIVO)->find($id);
+        $categorias = Categoria::where('activo', Categoria::ACTIVO)->get();
 
-        $categoriaEspecifica = Categoria::find($producto->id_categoria);
+        $categoriaEspecifica = Categoria::where('activo', Categoria::ACTIVO)->find($producto->id_categoria);
 
         return view('frontend.paginas.productoDetalle', compact('producto', 'categorias', 'categoriaEspecifica'));
     }
 
     public function filtrarPorPrecio(Request $request)
     {
-        $categorias = Categoria::get();
+        $categorias = Categoria::where('activo', Categoria::ACTIVO)->get();
 
 
         $precio_range = $request->input('precio_range', 0);
 
-        $productos = Producto::where('precio', '<=', $precio_range)
+        $productos = Producto::where('activo', Producto::ACTIVO)
+            ->where('precio', '<=', $precio_range)
             ->orderBy('precio', 'asc')
             ->simplePaginate(12)
             ->withQueryString();
