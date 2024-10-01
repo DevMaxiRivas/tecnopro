c233758fbea74c2f15e66a66ba61b386132e43db
AlejandraArratia <anarratia16@gmail.com>
2024-10-01 00:56:10 -0300

    nuevoscambios

--- routes/panel.php
+++ routes/panel.php
@@ -4,17 +4,9 @@
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoriaController;
 
 Route::get('/', function() {
     return view('panel.index');
 });
 Route::resource('/categorias', CategoriaController::class)->names('categoria');
 Route::resource('/productos', ProductoController::class)->names('producto');
-
-<<<<<<< HEAD
-=======
-
-Route::resource('/categorias', CategoriaController::class)->names('categoria');
-Route::resource('/productos', ProductoController::class)->names('producto');
->>>>>>> a792bed8e172c8acb0a4996013a2be374639c03c
