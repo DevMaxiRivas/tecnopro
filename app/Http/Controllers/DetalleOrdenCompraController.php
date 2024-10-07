20aaed5db44dec8bd858262b8bd874a5e96698df
MR PC <maximilianorivas01@gmail.com>
2024-10-07 17:50:43 -0300

    Quitar precios al agregar prod a OC

--- app/Http/Controllers/DetalleOrdenCompraController.php
+++ app/Http/Controllers/DetalleOrdenCompraController.php
@@ -45,8 +45,6 @@ class DetalleOrdenCompraController extends Controller
             $detalle = new DetalleCompra();
             $detalle->id_compra = $orden_compra->id;
             $detalle->id_producto = (int) $producto['id_producto'];
-            $detalle->precio = Producto::find((int) $producto['id_producto'])->precio;
-            $detalle->subtotal = Producto::find((int) $producto['id_producto'])->precio * (int) $producto['cantidad'];
             $detalle->cantidad = (int) $producto['cantidad'];
             $detalle->save();
         }
@@ -55,45 +53,4 @@ class DetalleOrdenCompraController extends Controller
         return response()->json(['success' => true, 'productos_por_agregar' => $detalle, 'orden_compra_id' => $orden_compra_id, 'detalles' => $detalles]);
     }
 
-    // public function guardarDetallesCompras(Request $request)
-    // {
-    //     $orden_compra_id = $request->get('orden_compra_id');
-    //     $orden_compra = Compra::find($orden_compra_id);
-
-    //     $detalles = $request->get('productos_cargados');
-    //     $productos_por_agregar = $request->get('productos_por_agregar');
-
-    //     // Actualizar detalles existentes
-    //     foreach ($detalles as $detalleData) {
-    //         $detalle = DetalleCompra::find((int) $detalleData['id_detalle']);
-
-    //         // Asegurarse de que el detalle existe antes de actualizar
-    //         if ($detalle) {
-    //             $detalle->cantidad = (int) $detalleData['cantidad'];
-    //             $detalle->save(); // Usa save() en lugar de update()
-    //         }
-    //     }
-
-    //     // Agregar nuevos productos a la orden de compra
-    //     foreach ($productos_por_agregar as $productoData) {
-    //         $producto = Producto::find((int) $productoData['id_producto']);
-
-    //         if ($producto) { // Asegurarse de que el producto existe
-    //             $detalle = new DetalleCompra();
-    //             $detalle->id_compra = $orden_compra->id;
-    //             $detalle->id_producto = $producto->id;
-    //             $detalle->precio = $producto->precio;
-    //             $detalle->cantidad = (int) $productoData['cantidad'];
-    //             $detalle->subtotal = $producto->precio * $detalle->cantidad;
-    //             $detalle->save();
-    //         }
-    //     }
-
-    //     return response()->json([
-    //         'success' => true,
-    //         'productos_por_agregar' => $productos_por_agregar,
-    //         'orden_compra_id' => $orden_compra_id,
-    //         'detalles' => $detalles
-    //     ]);
-    // }
 }
